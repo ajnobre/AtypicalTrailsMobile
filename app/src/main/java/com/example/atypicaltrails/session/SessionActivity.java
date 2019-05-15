21e390eb3063f30c4ecac96d678a42a5c71225df
ajnobre <alvarojnobre@gmail.com>
2019-05-15 18:25:00 +0100

    Transparent Menu

--- app/src/main/java/com/example/atypicaltrails/session/SessionActivity.java
+++ app/src/main/java/com/example/atypicaltrails/session/SessionActivity.java
@@ -1,13 +1,16 @@
 package com.example.atypicaltrails.session;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.example.atypicaltrails.R;
 
 public class SessionActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,19 @@ public class SessionActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
